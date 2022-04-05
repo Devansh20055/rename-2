9329a7be3b63676eeffc99ff5e71dc246e2ea22a
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-04-05 15:31:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,14 +12,14 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "6504201"))
+    API_HASH = os.environ.get("API_HASH", "dde01d26752d3420532c8f15510d0e72")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5288671031:AAF07G843q848oqAvMgboy6H7JEI_HsPppY")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID",1923922961))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://gdriveuploader12:gdriveop@cluster0.ftwj2.mongodb.net/Cluster0?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001605524352"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
